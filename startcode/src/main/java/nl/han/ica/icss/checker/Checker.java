da6602267c66b02d7da8b7c729921e8d202f06b7
Daaaniell <sungdaniel20@gmail.com>
2026-03-26 20:12:29 +0100

    checker variables

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -3,6 +3,7 @@ package nl.han.ica.icss.checker;
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -12,62 +13,84 @@ public class Checker {
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-         variableTypes = new HANLinkedList<>();
-         checkStylesheet(ast.root);
+        variableTypes = new HANLinkedList<>();
+        addNewScope();
+        walk(ast.root);
+        removeFirstScope();
     }
 
-    private void checkStylesheet(Stylesheet node) {
-        variableTypes.addFirst(new HashMap<>());
+    //wip
+
+    //1. loop door de boom
+    //2. check de type van de child
+    //3. is ene varaibel? zet in firsthashmap
+    //4. is ene reference? check de first hashmap
+
+    private void walk(ASTNode node) {
+        if (node instanceof Stylerule || node instanceof IfClause || node instanceof ElseClause) {
+            addNewScope();
+        }
+
         for (ASTNode child : node.getChildren()) {
-            if (child instanceof VariableAssignment) checkVariables((VariableAssignment) child);
-            if (child instanceof Stylerule) checkStylerules((Stylerule) child);
-        }
-        variableTypes.removeFirst();
-    }
-
-    private void checkStylerules(Stylerule stylerule) {
-//        variableTypes.addFirst(new HashMap<>());
-        for (ASTNode child : stylerule.getChildren()) {
-            if (child instanceof Declaration) checkDeclaration((Declaration) child);
-            if (child instanceof IfClause) checkIfClause((IfClause) child);
-        }
-//        variableTypes.removeFirst();
-    }
-
-    private void checkDeclaration(Declaration declaration) {
-        //TODO
-        checkExpression(declaration.expression);
-
-
-    }
-
-    private void checkExpression(Expression expression) {
-        if (expression instanceof VariableReference) {
-            if (!searchVariable(((VariableReference) expression).name)) {
-                expression.setError("bestaat niet");
+            if (child instanceof VariableAssignment) {
+                addVariableToScope((VariableAssignment) child);
+                checkExpression((child));
+            } else {
+                checkExpression(child);
             }
+
+            walk(child);
+        }
+
+        if (node instanceof Stylerule || node instanceof IfClause || node instanceof ElseClause) {
+            removeFirstScope();
         }
     }
 
-    private void checkIfClause(IfClause ifClause) {
-        //TODO
+    private void checkExpression(ASTNode expr) {
+        if (expr instanceof VariableReference) {
+            checkVariableReference((VariableReference) expr);
+        }
+        for (ASTNode child : expr.getChildren()) {
+            checkExpression(child);
+        }
     }
 
-    private void checkVariables(VariableAssignment variableAssignment) {
-        HashMap map = variableTypes.getFirst();
-        System.out.println(variableAssignment.getChildren());
+    // ------------------- helper functies -------------------
 
+//    private void isVariableInScope(VariableAssignment variableAssignment) {
+//        if (searchVariable(variableAssignment.name.name)) {
+//            variableAssignment.setError(variableAssignment.name + " bestaat niet");
+//        }
+//    }
+
+    private void checkVariableReference(VariableReference ref) {
+        if (!searchVariable(ref.name)) {
+            ref.setError(ref.name + " bestaat niet");
+        }
+    }
+    private void addVariableToScope(VariableAssignment variableAssignment) {
+        HashMap map = getFirstScope();
         String variableName = variableAssignment.name.name;
-        System.out.println("naam: " + variableName);
-
         map.put(variableName, variableAssignment);
     }
 
     private boolean searchVariable(String key) {
-        for (int current = 0; current < variableTypes.getSize(); current++) {
-            HashMap map = variableTypes.get(current);
-            if (map.containsKey(key)) return true;
+        for (int i = 0; i < variableTypes.getSize(); i++) {
+            if (variableTypes.get(i).containsKey(key)) return true;
         }
         return false;
+    }
+
+    private void addNewScope() {
+        variableTypes.addFirst(new HashMap<>());
+    }
+
+    private void removeFirstScope() {
+        variableTypes.removeFirst();
+    }
+
+    private HashMap getFirstScope() {
+        return variableTypes.getFirst();
     }
 }
