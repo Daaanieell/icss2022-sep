c59de36bfbfe7c0448891735d1b487f53eb879c6
Daaaniell <sungdaniel20@gmail.com>
2026-03-30 12:25:46 +0200

    generator af?

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,15 +1,62 @@
 package nl.han.ica.icss.generator;
 
 
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.PercentageLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
+import nl.han.ica.icss.ast.literals.ScalarLiteral;
 
 public class Generator {
 
+	//TODO https://www.geeksforgeeks.org/java/stringbuilder-class-in-java-with-examples/
+	StringBuilder sb = new StringBuilder();
+
 	public String generate(AST ast) {
-        return "";
-
-
+        return createCSS(ast);
 	}
 
-	
+	private String createCSS(AST ast) {
+		generateStylesheet(ast.root);
+		System.out.println(sb.toString());
+		return sb.toString();
+	}
+
+	private void generateStylesheet(Stylesheet stylesheet) {
+		for (ASTNode rule : stylesheet.body) {
+			if (rule instanceof Stylerule) generateStylerule((Stylerule) rule);
+		}
+	}
+
+	private void generateStylerule(Stylerule stylerule) {
+		String selector = stylerule.selectors.getFirst().toString();
+
+		sb.append(selector)
+			.append(" {\n");
+
+		for (ASTNode declaration : stylerule.body) {
+			if (declaration instanceof Declaration) generateDeclaration((Declaration) declaration);
+		}
+
+		sb.append("}\n\n");
+	}
+
+	private void generateDeclaration (Declaration declaration) {
+        Expression expression = declaration.expression;
+
+        sb.append("  ")
+			.append(declaration.property.name)
+			.append(": ")
+			.append(literalToString(expression))
+			.append("; \n");
+	}
+
+	private String literalToString(Expression expression) {
+		if (expression instanceof PixelLiteral px)       return px.value + "px";
+		if (expression instanceof PercentageLiteral pct) return pct.value + "%";
+		if (expression instanceof ColorLiteral col)      return col.value;
+		if (expression instanceof ScalarLiteral sc)      return String.valueOf(sc.value);
+		return "";
+	}
+
 }
